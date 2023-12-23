3d409929cd708a217e4739d5aab411cdecdeb68b
Sourav Kumar <souravkr.62@gmail.com>
2023-12-23 22:04:10 +0530

    Navbar implementation done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components"
 
 const App = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="min-h-screen"> {/* Mininum height as screen size */}
+        <div className="gradient-bg-welcome">
+          <Navbar />
+          <Welcome />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
+      </div>
     </>
   )
 }
