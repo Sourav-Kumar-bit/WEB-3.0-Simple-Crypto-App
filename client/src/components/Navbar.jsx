b546b92bb20d00552c8ebf0d003b984634731281
Sourav Kumar <souravkr.62@gmail.com>
2023-12-23 23:08:32 +0530

    Layout for welcome and loader done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-3 pb-1 pt-6">
+    <nav className="w-full flex md:justify-center justify-between items-center p-3">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
